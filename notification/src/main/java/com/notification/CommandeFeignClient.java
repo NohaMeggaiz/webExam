316564681da76a4e1d22b60d4b41c7a2aafc1036
noha <nohameggaiz@gmail.com>
2024-12-24 12:16:08 +0100

    adding  configurations2

--- notification/src/main/java/com/notification/CommandeFeignClient.java
+++ notification/src/main/java/com/notification/CommandeFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "commandes-service", url = "http://localhost:8082/api/commandes")
+@FeignClient(name = "commandes-service", url = "http://localhost:8222/api/commandes")
 public interface CommandeFeignClient {
 
     @GetMapping("/{id}")
